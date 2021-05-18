ca4c3b170e9583faf18f157a819b8dc82c05d206
sagar <sagar.sadhu3@gmail.com>
2021-05-18 11:45:25 +0530

    Password generator day-2 p Completed

--- django/password_generator-project/generator/views.py
+++ django/password_generator-project/generator/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hi Balu")
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvxyz')
+    if request.GET.get('uppercase'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('special'):
+        characters.extend('~!@#$%^&*()')
+    if request.GET.get('numbers'):
+        characters.extend('1234567890')
+    the_password = ''
+    length = int(request.GET.get('length', 12))
+    for item in range(length):
+        the_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': the_password})
+
