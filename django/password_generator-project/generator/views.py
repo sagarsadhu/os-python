b39d8f04e11495328ce68ccfd48d929954022234
sagar <sagar.sadhu3@gmail.com>
2021-05-20 10:00:07 +0530

    Ran black and pylint and did the changes

--- django/password_generator-project/generator/views.py
+++ django/password_generator-project/generator/views.py
@@ -1,26 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import random
+"""
+This is the views page where all the routes are present
+"""
 
+import random
+from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'generator/home.html')
+    """
+    this is the home page where you can select parameters to generate a password
+    :param request:
+    :return: HTML page
+    """
+    return render(request, "generator/home.html")
 
 
 def password(request):
-    characters = list('abcdefghijklmnopqrstuvxyz')
-    if request.GET.get('uppercase'):
-        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    if request.GET.get('special'):
-        characters.extend('~!@#$%^&*()')
-    if request.GET.get('numbers'):
-        characters.extend('1234567890')
-    the_password = ''
-    length = int(request.GET.get('length', 12))
-    for item in range(length):
+    """
+    this is the password page where the password is generated and displayed on the screen
+    :param request:
+    :return: HTML page
+    """
+    characters = list("abcdefghijklmnopqrstuvxyz")
+    if request.GET.get("uppercase"):
+        characters.extend("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    if request.GET.get("special"):
+        characters.extend("~!@#$%^&*()")
+    if request.GET.get("numbers"):
+        characters.extend("1234567890")
+    the_password = ""
+    length = int(request.GET.get("length", 12))
+    loop_length = 0
+    while loop_length < length:
         the_password += random.choice(characters)
+        loop_length += 1
 
-    return render(request, 'generator/password.html', {'password': the_password})
-
+    return render(request, "generator/password.html", {"password": the_password})
--- django/password_generator-project/password_generator/urls.py
+++ django/password_generator-project/password_generator/urls.py
@@ -17,6 +17,6 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('password/', views.password, name="password"),
+    path("", views.home, name="home"),
+    path("password/", views.password, name="password"),
 ]
