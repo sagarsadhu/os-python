48a992597ac4fd747ad438ca02eee5eb22cad0a2
sagar <sagar.sadhu3@gmail.com>
2021-05-21 11:56:18 +0530

    Personal Portfolio day-2 p

--- django/personal_portfolio-project/blog/models.py
+++ django/personal_portfolio-project/blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
