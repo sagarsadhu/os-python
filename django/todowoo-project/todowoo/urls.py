6bbf481f0ff8ca0f4489d81fd58c5371c445ac3f
sagar <sagar.sadhu3@gmail.com>
2021-05-26 11:47:47 +0530

    Todowoo day-4 p completed everything as well as styling

--- django/todowoo-project/todowoo/urls.py
+++ django/todowoo-project/todowoo/urls.py
@@ -21,12 +21,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Auth
-    path('signup/', views.signupuser, name='signup'),
+    path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
     path('login/', views.loginuser, name='loginuser'),
 
     # todos
     path('', views.home, name='home'),
-    path('current/', views.currenttodos, name='currenttodos')
+    path('create/', views.createtodo, name='createtodo'),
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo')
 
 ]
