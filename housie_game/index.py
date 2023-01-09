1104bc4d83f2855d09507c286faaf727417fb259
sagarsadhu <sagar.sadhu3@gmail.com>
2023-01-09 11:41:31 +0530

    Housie game and star

--- housie_game/index.py
+++ housie_game/index.py
@@ -1,12 +1,12 @@
 import random
 
-print('Welcome to the game of housie. Hope you have fun !!! ')
+print("Welcome to the game of housie. Hope you have fun !!! ")
 
 # number of players to generate cards
 game_check = False
 while True:
 
-    players = input('Please enter the number of players : ')
+    players = input("Please enter the number of players : ")
 
     if players.isdigit() is True:
         players = int(players)
@@ -15,8 +15,7 @@ while True:
         else:
             game_check = True
         break
-    else:
-        print("Please enter a valid number")
+    print("Please enter a valid number")
 
 
 if game_check is True:
@@ -24,15 +23,15 @@ if game_check is True:
     cards = {}
     housie_range = range(1, 101)
     for player_num in range(1, players + 1):
-        cards[f'player_{player_num}'] = random.sample(housie_range, 15)
+        cards[f"player_{player_num}"] = random.sample(housie_range, 15)
 
     for player, card in cards.items():
-        print('\n')
+        print("\n")
         print(player)
         print(*card[:5], sep=" ")
         print(*card[5:10], sep=" ")
         print(*card[10:], sep=" ")
-        print('\n')
+        print("\n")
 
 
 # zaldi5 check
@@ -47,9 +46,11 @@ def zaldi5_check(housie_numbers):
         return check, None, []
     return check, player, intersection
 
+
 # subset check
 def subset_check(list1, list2):
     return set(list1).issubset(set(list2))
+
 
 # row check
 def row_check(row_number, housie_numbers):
@@ -75,6 +76,7 @@ def row_check(row_number, housie_numbers):
         return check, None, []
     return check, player, row
 
+
 # row check
 def full_housie_check(housie_numbers):
     check = False
@@ -86,7 +88,6 @@ def full_housie_check(housie_numbers):
     if check is False:
         return check, None, []
     return check, player, card
-
 
 
 if game_check is True:
@@ -105,53 +106,57 @@ while game_check:
     print(f"The current housie number is {current_housie_num}")
     popped_housie_numbers.append(current_housie_num)
 
-    if len(popped_housie_numbers) >=5:
+    if len(popped_housie_numbers) >= 5:
 
         # zaldi5 check
         if players_zaldi5_check is False:
-            players_zaldi5_check, zaldi5_player, intersection = zaldi5_check(popped_housie_numbers)
+            players_zaldi5_check, zaldi5_player, intersection = zaldi5_check(
+                popped_housie_numbers
+            )
             if zaldi5_player is not None:
-                print(f'Congrats to {zaldi5_player} on finishing zaldi5 !!!')
-                print(f'Zaldi5 numbers are {intersection}')
+                print(f"Congrats to {zaldi5_player} on finishing zaldi5 !!!")
+                print(f"Zaldi5 numbers are {intersection}")
 
         # first row check
         if player_first_row_check is False:
-            player_first_row_check, row1_player, row1 = row_check(1, popped_housie_numbers)
+            player_first_row_check, row1_player, row1 = row_check(
+                1, popped_housie_numbers
+            )
             if row1_player is not None:
-                print(f'Congrats to {row1_player} on finishing Row 1 !!!')
-                print(f'Row 1 numbers are {row1}')
+                print(f"Congrats to {row1_player} on finishing Row 1 !!!")
+                print(f"Row 1 numbers are {row1}")
 
         # second row check
         if player_second_row_check is False:
-            player_second_row_check, row2_player, row2 = row_check(2, popped_housie_numbers)
+            player_second_row_check, row2_player, row2 = row_check(
+                2, popped_housie_numbers
+            )
             if row2_player is not None:
-                print(f'Congrats to {row2_player} on finishing Row 2 !!!')
-                print(f'Row 2 numbers are {row2}')
-        
+                print(f"Congrats to {row2_player} on finishing Row 2 !!!")
+                print(f"Row 2 numbers are {row2}")
+
         # third row check
         if player_third_row_check is False:
-            player_third_row_check, row3_player, row3 = row_check(3, popped_housie_numbers)
+            player_third_row_check, row3_player, row3 = row_check(
+                3, popped_housie_numbers
+            )
             if row3_player is not None:
-                print(f'Congrats to {row3_player} on finishing Row 3 !!!')
-                print(f'Row 3 numbers are {row3}')
-
+                print(f"Congrats to {row3_player} on finishing Row 3 !!!")
+                print(f"Row 3 numbers are {row3}")
 
     # full housie check
-    if len(popped_housie_numbers) >=15:
-        player_full_housie_check , full_housie_player, card = full_housie_check(popped_housie_numbers)
+    if len(popped_housie_numbers) >= 15:
+        player_full_housie_check, full_housie_player, card = full_housie_check(
+            popped_housie_numbers
+        )
         if full_housie_player is not None:
-            print(f'Congrats to {full_housie_player} on finishing game !!!')
-            print(f'card numbers are {card}')
-            game_check= False
+            print(f"Congrats to {full_housie_player} on finishing game !!!")
+            print(f"card numbers are {card}")
+            game_check = False
 
-
-
-
-
-    text = input('Housie input : ')
+    text = input("Housie input : ")
     if text != "next":
-        game_check= False
-        
+        game_check = False
 
 
-print('Thanks for playing the game')
+print("Thanks for playing the game")
